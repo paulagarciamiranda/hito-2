20e065f181549134187314b47532c34202ad0767
Paula García <pdgarciamiranda@gmail.com>
2024-09-29 16:15:07 -1200

    registro listo

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Footer from "./components/Footer"
+import Register from "./components/Register"
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Home/>
+      {/* <Home/> */}
+      <Register/>
       <Footer/>
     </>
   )
